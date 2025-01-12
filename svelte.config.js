4c7b5097e6be8ee22664f98a9c4cd6ad0db59681
kulaizki <kulaizke@gmail.com>
2025-01-12 18:34:48 +0800

    feat: add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter({
-			runtime: 'nodejs20.x'
+			runtime: 'nodejs20.x' 
 		})
 	}
 };
