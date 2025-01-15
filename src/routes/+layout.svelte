033a2399d3947a82b49e1def122502d3fff7d7a8
kulaizki <kulaizke@gmail.com>
2025-01-15 12:42:41 +0800

    feat: add global metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,14 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<svelte:head>
+	<title>Fitz Angelo</title>
+	<meta name="description" content="Welcome to Fitz's developer portfolio, showcasing experience in software engineering, mobile development, and UI/UX design." />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta charset="UTF-8" />
+	<link rel="icon" href="/favicon.ico" />
+</svelte:head>
+
+<main>
+	{@render children()}
+</main>
