f0f6fd2d7eb228bc9c85a996befd37f7a6241bfb
kulaizki <kulaizke@gmail.com>
2025-01-15 12:19:43 +0800

    feat: add TechItem component

---
+++ src/lib/components/TechItem.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let techName = '';
+</script>
+
+<div class="px-4 py-2 bg-gray-800 rounded-lg shadow-md hover:bg-blue-500 hover:text-white transition-all duration-300">
+  {techName}
+</div>
