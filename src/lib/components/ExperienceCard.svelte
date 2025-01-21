57373b924b0c4b7ef6ffa41696a6e398ea6e05ca
kulaizki <kulaizke@gmail.com>
2025-01-21 23:26:18 +0800

    feat: add hover glow effect to ExperienceCard

--- src/lib/components/ExperienceCard.svelte
+++ src/lib/components/ExperienceCard.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-  export let title: string;
-  export let company: string;
-  export let date: string;
-  export let responsibilities: string[] = [];
-  export let link: string = '';
+	export let title: string;
+	export let company: string;
+	export let date: string;
+	export let responsibilities: string[] = [];
+	export let link: string = '';
 </script>
 
-<a 
-  href={link} 
-  target="_blank" 
-  class="relative p-6 bg-gray-800 rounded-lg shadow-lg border-t-4 border-gray-500 border-gray-700 hover:bg-gray-700 hover:border-sky-400 hover:shadow-xl transition-all duration-300"
->
-  <h3 class="text-xl font-semibold text-white">{title}</h3>
-  <p class="text-sm text-gray-400 mt-2">{company}, {date}</p>
-  <ul class="mt-3 space-y-2 text-gray-300 text-sm">
-    {#each responsibilities as responsibility}
-      <li>• {responsibility}</li>
-    {/each}
-  </ul>
+<a
+	href={link}
+	target="_blank"
+  class="relative rounded-lg border-t-4 border-gray-500 border-gray-700 bg-gray-800 p-6 shadow-lg transition-all delay-50 duration-300 ease-in-out hover:border-sky-400 hover:bg-gray-700 hover:shadow-[0_0_15px_rgba(56,189,248,0.7)]"
+  >
+	<h3 class="text-xl font-semibold text-white">{title}</h3>
+	<p class="mt-2 text-sm text-gray-400">{company}, {date}</p>
+	<ul class="mt-3 space-y-2 text-sm text-gray-300">
+		{#each responsibilities as responsibility}
+			<li>• {responsibility}</li>
+		{/each}
+	</ul>
 </a>
