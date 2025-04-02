ee36f60d18dbcd5f40bcb0a48b3b39bcc8f20cc5
kulaizki <kulaizke@gmail.com>
2025-04-02 16:59:06 +0800

    feat: add Projects component to showcase personal projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import Hero from './Hero.svelte';
   import Experience from './Experience.svelte';
   import Skills from './Skills.svelte';
+  import Projects from './Projects.svelte';
   import Footer from './Footer.svelte';
   import Contact from './Contact.svelte';
   import ScrollToTop from '$lib/components/ScrollToTop.svelte';
@@ -12,6 +13,7 @@
 <div class="bg-gradient-to-br from-gray-950 via-gray-800 to-black">
   <Experience className="border-y border-gray-700 pb-16" />
   <Skills className="border-b border-gray-700 pb-16" />
+  <Projects className="border-b border-gray-700 pb-16" />
   <Contact className="" />
 </div>
 
