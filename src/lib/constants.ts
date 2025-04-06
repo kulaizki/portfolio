47caf976c4b501b97af8e7f30d4d501755f7b889
kulaizki <kulaizke@gmail.com>
2025-04-06 09:43:37 +0800

    feat: add imageUrl property to Project interface and update ProjectItem component to display project images

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,18 +3,21 @@ export const projects = [
     name: 'nexleague',
     description:
       'An AI-powered League of Legends profile analyzer providing personalized performance insights & coaching.',
-    url: 'https://nexleague.vercel.app/'
+    url: 'https://nexleague.vercel.app/',
+    imageUrl: '/images/projects/nexleague-1.png'
   },
   {
     name: 'z1',
     description:
       'An AI-powered Dota 2 profile analyzer providing personalized performance insights & coaching.',
-    url: 'https://z1-dota.vercel.app/'
+    url: 'https://z1-dota.vercel.app/',
+    imageUrl: '/images/projects/z1-1.png'
   },
   {
     name: 'Neurotypical vs. ASD',
     description:
       'An app that visualizes Neurotypical vs. Autism Brain Disorder Brain Connectivity using the Harvard-Oxford atlases.',
-    url: 'https://neurotypical-vs-asd.streamlit.app/'
+    url: 'https://neurotypical-vs-asd.streamlit.app/',
+    imageUrl: '/images/projects/neuro-vs-asd-1.png'
   }
 ]; 
