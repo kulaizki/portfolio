4e9e1768f501f5e49c55ba870b702e0891ab8462
kulaizki <kulaizke@gmail.com>
2025-04-06 15:09:45 +0800

    fix: adjust padding in skill-item.svelte for improved layout and visual consistency

--- src/lib/components/skill-item.svelte
+++ src/lib/components/skill-item.svelte
@@ -8,7 +8,7 @@
   href={`https://www.google.com/search?q=${skill.name}`} 
   target="_blank" 
   rel="noopener noreferrer"
-  class="flex transform flex-col items-center justify-center rounded-lg bg-gray-700 p-4 shadow-md transition duration-300 ease-in-out hover:-translate-y-1 hover:bg-gray-600"
+  class="flex transform flex-col items-center justify-center rounded-lg bg-gray-700 p-2 md:p-3 shadow-md transition duration-300 ease-in-out hover:-translate-y-1 hover:bg-gray-600"
   title={skill.name}
 >
   <img src={skill.icon} alt={skill.name} class="h-10 w-10 object-contain rounded-lg" />
