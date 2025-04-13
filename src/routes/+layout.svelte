0eca7497817e2721be279e1109f49f697e48b507
kulaizki <kulaizke@gmail.com>
2025-04-13 18:03:35 +0800

    chore: update meta description in layout.svelte for improved clarity

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 
 <svelte:head>
 	<title>Fitzsixto</title>
-	<meta name="description" content="Explore Fitz's developer portfolio featuring expertise in software engineering, mobile app development, and UI/UX design." />
+	<meta name="description" content="Explore Fitz's portfolio featuring expertise in multiple skills." />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta charset="UTF-8" />
 	<link rel="icon" href="/images/favicon.png" />
