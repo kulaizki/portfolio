e1fe5fea94f68f6cba7e359daa19ebf9ceece25e
kulaizki <kulaizke@gmail.com>
2025-04-13 17:35:05 +0800

    chore: update name references from 'Fitz Angelo' to 'Fitzsixto' in layout and hero components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <svelte:head>
-	<title>Fitz Angelo</title>
+	<title>Fitzsixto</title>
 	<meta name="description" content="Explore Fitz's developer portfolio featuring expertise in software engineering, mobile app development, and UI/UX design." />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta charset="UTF-8" />
